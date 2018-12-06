2b9b3a85487e893a3bf401be93f17ac6590ed191
Furkan Türkal <furkan.turkal@hotmail.com>
2018-12-06 13:51:28 +0300

    [fix] array-smallest-missing-number parameters

--- array-smallest-missing-number/array-smallest-missing-number.go
+++ array-smallest-missing-number/array-smallest-missing-number.go
@@ -8,29 +8,34 @@
 package main
 
 import "fmt"
+import "os"
 
-func findFirstMissing (arr []int, start, end int) int {
-	
-	if(start > end) {
+func findFirstMissing(arr []int, start, end int) int {
+	if start < 0 {
+		fmt.Println("Start must be greater than 0")
+		os.Exit(1)
+		return 0
+	}
+
+	if start > end {
 		return end + 1
 	}
 
-	if(start != arr[start]) {
+	if start != arr[start] {
 		return start
 	}
 
 	mid := (start + end) / 2
 
-	if(arr[mid] == mid){
-		return findFirstMissing(arr, mid + 1, end)
+	if arr[mid] == mid {
+		return findFirstMissing(arr, mid+1, end)
 	}
 
 	return findFirstMissing(arr, start, end)
 }
 
-
 func main() {
 	arr := []int{0, 1, 2, 3, 4, 5, 6, 7, 10}
 	var n int = len(arr)
-	fmt.Printf("Smallest missing element is %d", findFirstMissing(arr, 0, n - 1))
+	fmt.Printf("Smallest missing element is %d", findFirstMissing(arr, 0, n-1))
 }
