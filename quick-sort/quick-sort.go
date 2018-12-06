67909efa60b6cb45169c8e7f9ef0d72d9cde848f
Furkan Türkal <furkan.turkal@hotmail.com>
2018-12-06 14:06:01 +0300

    [fix] code format for quick-sort

--- quick-sort/quick-sort.go
+++ quick-sort/quick-sort.go
@@ -9,51 +9,50 @@ package main
 
 import "fmt"
 
-
-func Swap(a *int, b *int){
+func Swap(a *int, b *int) {
 	t := *a
 	*a = *b
 	*b = t
 }
 
 func Partition(arr []int, start, end int) int {
-	
+
 	pivot := arr[end]
 
 	//Index of smaller element
 	var i int = (start - 1)
 
-	for j := start; j <= end - 1; j++ {
+	for j := start; j <= end-1; j++ {
 		//If current element is smaller than or equal to pivot
-		if(arr[j] <= pivot){
+		if arr[j] <= pivot {
 			i++
 			Swap(&arr[i], &arr[j])
 		}
 	}
 
-	Swap(&arr[i + 1], &arr[end])
+	Swap(&arr[i+1], &arr[end])
 
 	return (i + 1)
 }
 
 /*The main function that implements QuickSort
-	arr[] -> Array to be sorted
-	start -> Starting index
-	end   -> Ending index
+arr[] -> Array to be sorted
+start -> Starting index
+end   -> Ending index
 */
 func QuickSort(arr []int, start, end int) {
-	if(start < end){
+	if start < end {
 		//pi is partitioning index, arr[p] is now at right place
 		var pi int = Partition(arr, start, end)
 
 		//Separately sort elements before partition and after partition
-		QuickSort(arr, start, pi - 1)
-		QuickSort(arr, pi + 1, end)
+		QuickSort(arr, start, pi-1)
+		QuickSort(arr, pi+1, end)
 	}
 }
 
-func PrintArray(arr []int, size int){
-	for i:=0; i < size; i++ {
+func PrintArray(arr []int, size int) {
+	for i := 0; i < size; i++ {
 		fmt.Printf("%d ", arr[i])
 	}
 	fmt.Printf("\n")
@@ -63,7 +62,7 @@ func main() {
 	arr := []int{10, 7, 8, 9, 1, 5}
 	var n int = len(arr)
 
-	QuickSort(arr, 0, n - 1)
+	QuickSort(arr, 0, n-1)
 
 	fmt.Println("Sorted array is: ")
 	PrintArray(arr, n)
